ac800c2fddfe4ce30d5218623d6ae04825579908
DEMNATI Anasse <demnati_anasse@emsi-edu.ma>
2022-09-12 19:16:50 +0100

    send email by mailhug

--- src/controller/forgot.controller.ts
+++ src/controller/forgot.controller.ts
@@ -1,15 +1,33 @@
 import { Reset } from './../entity/reset.entity';
 import { getRepository } from 'typeorm';
 import { Request, Response } from "express";
+import { createTransport } from 'nodemailer';
+
+
 
 export const Forgot = async (req:Request, res:Response) => {
     const {email} = req.body;
     const token = Math.random().toString(20).substring(2,12);
 
-    const reset = await getRepository(Reset).save({
+    await getRepository(Reset).save({
         email,
         token
     })
+    const transporter = createTransport({
+        host: "0.0.0.0",
+        port: 1025,
+    });
 
-    res.send(reset)
+    const url = `http://localhost:3000/reset/${token}`;
+
+    await transporter.sendMail({
+        from: 'from@exmple.com',
+        to: email,
+        subject: 'Reset password!',
+        html: `Click <a href="${url}">here</a> to  Reset your password`
+    })
+
+    res.send({
+        message: 'Check your email to reset your password'
+    })
 }
