752f7975dcdc4610745c69e301aa811315fe65ec
DEMNATI Anasse <demnati_anasse@emsi-edu.ma>
2022-09-12 11:31:40 +0100

    install express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("hello 3");
+ import  express,{Request,Response}  from "express";
+
+ const app = express();
+
+ app.use(express.json());// for parsing application/json
+
+ app.get('/', (req:Request, res:Response) => {
+    res.send('Hello World!');
+ })
+
+ app.listen(5000, () => {
+    console.log('Server started on port 5000');
+ }
+)
