a40d0261e09905b2331e570d8833c188f8c667e6
DEMNATI Anasse <demnati_anasse@emsi-edu.ma>
2022-09-12 19:40:56 +0100

    reset password

--- src/controller/forgot.controller.ts
+++ src/controller/forgot.controller.ts
@@ -1,11 +1,12 @@
+import { User } from './../entity/user.entity';
 import { Reset } from './../entity/reset.entity';
 import { getRepository } from 'typeorm';
 import { Request, Response } from "express";
 import { createTransport } from 'nodemailer';
+import bcryptjs from 'bcryptjs'
 
 
-
-export const Forgot = async (req:Request, res:Response) => {
+export const ForgotPassword = async (req:Request, res:Response) => {
     const {email} = req.body;
     const token = Math.random().toString(20).substring(2,12);
 
@@ -30,4 +31,47 @@ export const Forgot = async (req:Request, res:Response) => {
     res.send({
         message: 'Check your email to reset your password'
     })
+}
+
+export const ResetPassword = async (req:Request, res:Response) => {
+    const {token,password,password_confirm} = req.body;
+    if(req.body.password !== req.body.password_confirm){
+        return res.status(400)
+        .send({
+         message:"Password's do not match!"
+        }) 
+     }
+     const ResetPassword = await getRepository(Reset).findOne({
+            where:{
+                token
+            }
+        });
+    if(!ResetPassword){
+        return res.status(400).send({
+            message:'Invalide link !'
+        })
+    }
+
+    const user = await getRepository(User).findOne({
+        where:{
+            email:ResetPassword.email
+        }
+    });
+
+    if(!user){
+        return res.status(404).send({
+            message:'user not found !'
+        })
+    }
+    
+    await getRepository(User).update(user.id,{
+        password:await bcryptjs.hash(password,12)
+    })
+
+    await getRepository(Reset).delete(ResetPassword.id);
+    res.send({
+        message:'Password updated successfully'
+    })
+
+
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import { Forgot } from './controller/forgot.controller';
+import { ForgotPassword, ResetPassword } from './controller/forgot.controller';
 import { Login, Register, AuthenticatedUser, Refresh, Logout } from './controller/auth.controller';
 import { Router } from "express";
 
@@ -8,6 +8,8 @@ export const routes = (router:Router)=>{
     router.get('/api/user',AuthenticatedUser);
     router.post('/api/refresh',Refresh);
     router.post('/api/logout',Logout);
-    router.post('/api/forgot',Forgot);
+    router.post('/api/forgot',ForgotPassword);
+    router.post('/api/reset',ResetPassword);
+
 
 }
