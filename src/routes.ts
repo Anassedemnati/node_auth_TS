4c99d94b2e777b968dbec168ff65c82475e6f7ac
DEMNATI Anasse <demnati_anasse@emsi-edu.ma>
2022-09-12 13:08:09 +0100

    done login method wihout token

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
-import { Register } from './controller/auth.controller';
+import { Login, Register } from './controller/auth.controller';
 import { Router } from "express";
 
 export const routes = (router:Router)=>{
-    router.post('/api/register',Register)
+    router.post('/api/register',Register);
+    router.post('/api/login',Login);
 }
